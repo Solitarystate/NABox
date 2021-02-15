f108233d40632674ff39dc15318b1e2a28845463
Solitarystate <solitarystate@hotmail.com>
2021-02-15 13:45:08 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+host = "ipaddress"
+port = <portnumber. (22 for ssh)>
+username = "username"
+password = "password"
+command = "/opt/netapp-harvest/netapp-manager -status"
+token = {'X-Token': 'someverylargetoken'}
+tickettoolurl = 'https://tickettoolapi.company.com/'
